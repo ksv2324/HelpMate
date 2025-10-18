a7738e0b769ade3632ae98eff00695d9ff15ea68
AjayAntoIsDev <ajayantoisdev@gmail.com>
2025-10-18 17:04:37 +0530

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
-  export default defineConfig({
+  export default defineConfig(({ command }) => ({
+    base: command === 'build' ? '/HelpMate/' : '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -65,4 +66,5 @@
       port: 3000,
       open: true,
     },
-  });
+  },
+  ));
